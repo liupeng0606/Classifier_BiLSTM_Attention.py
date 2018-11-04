1aaf8fdf71d757a946154dafb90f8837fa0f8901
liupeng <liupeng@hist.edu.cn>
2018-11-04 13:37:59 +0800

    Update Classifier_BiLSTM_Attention.py

--- Classifier_BiLSTM_Attention.py
+++ Classifier_BiLSTM_Attention.py
@@ -1,27 +1,97 @@
 import numpy as np
 import pandas as pd
 import re
-
 from bs4 import BeautifulSoup
-
-import sys
-import os
-
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils.np_utils import to_categorical
+from keras.layers import Dense, Input
+from keras.layers import  Embedding, LSTM, Bidirectional
+from keras.models import  Model
+from keras import backend as K
+from keras.engine.topology import Layer
+from keras import initializers, regularizers, constraints
 
-from keras.layers import Masking
-from keras.layers import Dense, Input, Flatten
-from keras.layers import Conv1D, GlobalMaxPooling1D, Embedding, Merge, Dropout, LSTM, GRU, Bidirectional
-from keras.models import Sequential, Model
-from Attention_layer import Attention_layer
+
+class Attention_layer(Layer):
+
+    def __init__(self,
+                 W_regularizer=None, b_regularizer=None,
+                 W_constraint=None, b_constraint=None,
+                 bias=True, **kwargs):
+
+        self.supports_masking = True
+        self.init = initializers.get('glorot_uniform')
+
+        self.W_regularizer = regularizers.get(W_regularizer)
+        self.b_regularizer = regularizers.get(b_regularizer)
+
+        self.W_constraint = constraints.get(W_constraint)
+        self.b_constraint = constraints.get(b_constraint)
+
+        self.bias = bias
+        super(Attention_layer, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        assert len(input_shape) == 3
+
+        self.W = self.add_weight((input_shape[-1], input_shape[-1],),
+                                 initializer=self.init,
+                                 name='{}_W'.format(self.name),
+                                 regularizer=self.W_regularizer,
+                                 constraint=self.W_constraint)
+        if self.bias:
+            self.b = self.add_weight((input_shape[-1],),
+                                     initializer='zero',
+                                     name='{}_b'.format(self.name),
+                                     regularizer=self.b_regularizer,
+                                     constraint=self.b_constraint)
+
+        super(Attention_layer, self).build(input_shape)
+
+    def compute_mask(self, input, input_mask=None):
+        # do not pass the mask to the next layers
+        return None
+
+    def call(self, x, mask=None):
+        uit = K.dot(x, self.W)
+
+        if self.bias:
+            uit += self.b
+
+        uit = K.tanh(uit)
+
+        a = K.exp(uit)
+
+        # apply mask after the exp. will be re-normalized next
+        if mask is not None:
+            # Cast the mask to floatX to avoid float64 upcasting in theano
+            a *= K.cast(mask, K.floatx())
+
+        # in some cases especially in the early stages of training the sum may be almost zero
+        # and this results in NaN's. A workaround is to add a very small positive number to the sum.
+        # a /= K.cast(K.sum(a, axis=1, keepdims=True), K.floatx())
+        a /= K.cast(K.sum(a, axis=1, keepdims=True) + K.epsilon(), K.floatx())
+        print a
+        # a = K.expand_dims(a)
+        print x
+        weighted_input = x * a
+        print weighted_input
+        return K.sum(weighted_input, axis=1)
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], input_shape[-1])
+
+
 
 SENTENCE_NUM = 25000
 MAX_SEQUENCE_LENGTH = 1000
 MAX_NB_WORDS = 20000
 EMBEDDING_DIM = 100
 VALIDATION_SPLIT = 0.2
+
+
+
 
 def clean_str(string):
     """
@@ -33,9 +103,8 @@ def clean_str(string):
     string = re.sub(r"\"", "", string)
     return string.strip().lower()
 
-data_train = pd.read_csv('/data/mpk/IMDB/labeledTrainData.tsv', sep='\t')
+data_train = pd.read_csv('labeledTrainData.tsv', sep='\t')
 print data_train.shape
-
 texts = []
 labels = []
 
@@ -44,9 +113,8 @@ for idx in range(data_train.review.shape[0]):
     texts.append(clean_str(text.get_text().encode('ascii','ignore')))
     labels.append(data_train.sentiment[idx])
 
-GLOVE_DIR = "/data/mpk"
 embeddings_index = {}
-f = open(os.path.join(GLOVE_DIR, 'glove.6B.100d.txt'))
+f = open('glove.6B.100d.txt')
 for line in f:
     values = line.split()
     word = values[0]
@@ -56,28 +124,39 @@ f.close()
 
 print('Total %s word vectors.' % len(embeddings_index))
 
-labels = to_categorical(np.asarray(labels))
+labels = to_categorical(np.asarray(labels),num_classes=3)
 print('Shape of data tensor:', len(texts))
 print('Shape of label tensor:', len(labels))
+
 
 tokenizer = Tokenizer(nb_words=MAX_NB_WORDS)
 tokenizer.fit_on_texts(texts)
 sequences = tokenizer.texts_to_sequences(texts)
+
 word_index = tokenizer.word_index
+
+
 data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
 
 
-indices = np.arange(data.shape[0])
+indices = np.arange(data.shape[0])   # make a list ,lenght is 25000
 np.random.shuffle(indices)
 data = data[indices]
 labels = labels[indices]
 
+
+# SPLIT DATA , ONE PART FOR TRAIN , THE OTHER PART FOR PREDICT
 nb_validation_samples = int(VALIDATION_SPLIT * data.shape[0])
 x_train = data[:-nb_validation_samples]
 y_train = labels[:-nb_validation_samples]
 x_val = data[-nb_validation_samples:]
 y_val = labels[-nb_validation_samples:]
 
+
+print x_train.shape
+print y_train.shape
+print x_val.shape
+print y_val.shape
 
 embedding_matrix = np.random.random((len(word_index) + 1, EMBEDDING_DIM))
 for word, i in word_index.items():
@@ -86,32 +165,33 @@ for word, i in word_index.items():
         # words not found in embedding index will be all-zeros.
         embedding_matrix[i] = embedding_vector
 print ('Length of embedding_matrix:', embedding_matrix.shape[0])
+
 embedding_layer = Embedding(len(word_index) + 1,
                             EMBEDDING_DIM,
                             weights=[embedding_matrix],
                             mask_zero=False,
                             input_length=MAX_SEQUENCE_LENGTH,
                             trainable=False)
-
 print('Traing and validation set number of positive and negative reviews')
 print y_train.sum(axis=0)
 print y_val.sum(axis=0)
-
-
 
 sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
 embedded_sequences = embedding_layer(sequence_input)
 l_gru = Bidirectional(LSTM(100, return_sequences=True))(embedded_sequences)
 l_att = Attention_layer()(l_gru)
 dense_1 = Dense(100,activation='tanh')(l_att)
-dense_2 = Dense(2, activation='softmax')(dense_1)
-
+print ("************************************************")
+print dense_1.shape
+print ("************************************************")
+dense_2 = Dense(3, activation='softmax')(dense_1)
+print ("************************************************")
+print dense_2.shape
 model = Model(sequence_input, dense_2)
-
+print ("************************************************")
 model.compile(loss='categorical_crossentropy',
               optimizer='rmsprop',
-              metrics=['acc'])
-
+              metrics=['accuracy'])
 model.summary()
 model.fit(x_train, y_train, validation_data=(x_val, y_val),
 nb_epoch=10, batch_size=50)
